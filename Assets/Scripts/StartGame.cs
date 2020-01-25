1763540d2843fb777a314e625cd5085ef51a0849
Kiran Thomas Payne <kiran.t.goel@gmail.com>
2020-01-25 05:54:40 -0500

    Added TV, and light turns on after few seconds to start game

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class StartGame : MonoBehaviour
+{
+
+    private AudioSource flick = null;
+    private GameObject lamp = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        flick = GetComponent<AudioSource>();
+        flick.time = flick.clip.length * .25f;
+        lamp = GameObject.Find("Lamp");
+        Invoke("TurnOn", 5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void TurnOn()
+    {
+        lamp.GetComponent<Light>().intensity = 5;
+        flick.Play();
+    }
+}
