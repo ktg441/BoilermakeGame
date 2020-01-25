01ef982ef269f1f86099fa979bc86f61856e6ca9
Kiran Thomas Payne <kiran.t.goel@gmail.com>
2020-01-25 16:24:21 -0500

    Added grabbing script

---
+++ VRClient/Assets/Scripts/ButtonTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonTest : MonoBehaviour
+{
+
+    public GameObject test;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        test = GameObject.Find("TVset");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
